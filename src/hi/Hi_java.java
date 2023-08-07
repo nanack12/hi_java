263d1300abad97694ccb8dbc8b25001e0f80682f
nanack12 <ananack12@gmail.com>
2023-08-07 12:13:09 +0900

    수정1

--- src/hi/Hi_java.java
+++ src/hi/Hi_java.java
@@ -5,6 +5,7 @@ public class Hi_java {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("성공했네~~~ 기쁘네");
+		System.out.println("왜 안되냐 진짜 열받게하지마라");
 	}
 
 }
